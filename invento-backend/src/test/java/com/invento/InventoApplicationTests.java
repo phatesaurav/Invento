5c345b085e5d0dbf6a226dc438cbf5387bf55f7f
Saurav <phatesaurav@gmail.com>
2023-10-21 10:25:08 +0530

    initial commit

---
+++ invento-backend/src/test/java/com/invento/InventoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.invento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ invento-frontend/src/api/BaseURL.js
@@ -0,0 +1,2 @@
+const base_url = "http://localhost:8081/invento";
+export default base_url;
