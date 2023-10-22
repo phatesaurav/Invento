1b4334fd2e63b6caa12f152430a39577eae39613
Saurav <phatesaurav@gmail.com>
2023-10-23 05:25:41 +0530

    Update BaseURL.js

--- invento-frontend/src/api/BaseURL.js
+++ invento-frontend/src/api/BaseURL.js
@@ -1,4 +1,4 @@
 // const base_url = "http://localhost:8081/invento";
-const base_url = "https://kopwmz1.localto.net/invento";
+const base_url = "https://gd6fsfq.localto.net:8081/invento";
 // const base_url = "http://51.20.95.39:8081/invento";
 export default base_url;
