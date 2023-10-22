637dd9147ec7f5a90cdcfe470555976dd16484b8
Saurav <phatesaurav@gmail.com>
2023-10-23 03:29:05 +0530

    Update BaseURL.js to connect to AWS EC2

--- invento-frontend/src/api/BaseURL.js
+++ invento-frontend/src/api/BaseURL.js
@@ -1,3 +1,4 @@
 // const base_url = "http://localhost:8081/invento";
-const base_url = "https://50c640ea8fe7a0.lhr.life/invento";
+// const base_url = "https://50c640ea8fe7a0.lhr.life/invento";
+const base_url = "http://51.20.95.39:8081/invento";
 export default base_url;
