b063ffc68c6fe547b9d94642b1d43e6962986140
Saurav <102415445+phatesaurav@users.noreply.github.com>
2023-10-23 14:24:05 +0530

    Update BaseURL.js

--- invento-frontend/src/api/BaseURL.js
+++ invento-frontend/src/api/BaseURL.js
@@ -1,4 +1,4 @@
 // const base_url = "http://localhost:8081/invento";
-const base_url = "https://ecede46b93a92d.lhr.life/invento";
+const base_url = "https://01ca0f191d442f.lhr.life/invento";
 // const base_url = "http://51.20.95.39:8081/invento";
 export default base_url;
