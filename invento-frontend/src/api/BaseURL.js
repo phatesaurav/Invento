50a27fc0543e56ddc8e8993e63ba97ed1033031a
Saurav <102415445+phatesaurav@users.noreply.github.com>
2023-10-24 09:32:29 +0530

    Update BaseURL.js

--- invento-frontend/src/api/BaseURL.js
+++ invento-frontend/src/api/BaseURL.js
@@ -1,4 +1,4 @@
 // const base_url = "http://localhost:8081/invento";
-const base_url = "https://2a7f183cdcdb3a1e9a9e89120a3bf90d.serveo.net/invento";
+const base_url = "https://invento.serveo.net/invento";
 // const base_url = "http://51.20.95.39:8081/invento";
 export default base_url;
